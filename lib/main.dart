a4b3eed120a39698fd5b799d4121e2fa8006d04a
ETAvadh <developer.empiere@gmail.com>
2025-02-26 14:20:49 +0530

    - Screen Custom Info Window: Implemented custom info window on the map based on the static latlng markers. - pubspec: Added and registered the plugin to display custom info window.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_google_map/screen_google_map.dart';
+import 'package:flutter_google_map/screen_custom_info_window.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ScreenGoogleMap(),
+      home: const ScreenCustomInfoWindow(),
     );
   }
 }
---
+++ lib/screen_custom_info_window.dart
@@ -0,0 +1,103 @@
+import 'package:custom_info_window/custom_info_window.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class ScreenCustomInfoWindow extends StatefulWidget {
+  const ScreenCustomInfoWindow({super.key});
+
+  @override
+  State<ScreenCustomInfoWindow> createState() => _ScreenCustomInfoWindowState();
+}
+
+class _ScreenCustomInfoWindowState extends State<ScreenCustomInfoWindow> {
+  LatLng myCurrentLocation = const LatLng(22.294188, 70.789927);
+  final CustomInfoWindowController csInfoWindowController = CustomInfoWindowController();
+  Set<Marker> marker = {};
+
+  final List<LatLng> latLngPoints = [
+    const LatLng(22.289813, 70.794422),
+    const LatLng(22.294358, 70.815387),
+  ];
+
+  final List<String> locNames = ["Shree Ramkrishna Temple", "Shree Ramnath Mahadev"];
+
+  final List<String> locImages = [
+    "https://media-cdn.tripadvisor.com/media/photo-o/07/d9/05/6f/shri-ramakrishna-ashrama.jpg", // Shree Ramkrishna Temple
+    "https://i.pinimg.com/originals/5a/13/e3/5a13e3408e63d6a7f79675e50054df94.jpg", // Shree Ramnath Mahadev
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    displayInfo();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      body: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true,
+            compassEnabled: true,
+            initialCameraPosition: CameraPosition(
+              target: myCurrentLocation,
+              zoom: 12,
+            ),
+            markers: marker,
+            onMapCreated: (controller) {
+              csInfoWindowController.googleMapController = controller;
+            },
+          ),
+          CustomInfoWindow(
+            controller: csInfoWindowController,
+            height: 150,
+            width: 250,
+            offset: 40,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void displayInfo() {
+    for (int i = 0; i < latLngPoints.length; i++) {
+      marker.add(
+        Marker(
+          markerId: MarkerId(i.toString()),
+          icon: BitmapDescriptor.defaultMarker,
+          position: latLngPoints[i],
+          onTap: () {
+            csInfoWindowController.addInfoWindow!(
+              Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 2.0).copyWith(top: 2.0),
+                      child: Image.network(
+                        locImages[i],
+                        height: 125,
+                        width: 250,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Text(
+                      locNames[i],
+                      style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+              latLngPoints[i],
+            );
+          },
+        ),
+      );
+      setState(() {});
+    }
+  }
+}
